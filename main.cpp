42add0cddd63a823e156bd4b40ee4190f6eddbc1
arsen <arssenk@gmail.com>
2017-05-28 15:05:25 +0300

    Final version

--- main.cpp
+++ main.cpp
@@ -5,27 +5,15 @@
 #include <mutex>
 #include <thread>
 #include <map>
+
+
 #include <sstream>
 
 using namespace std;
 
 mutex mx;
 
-inline std::chrono::high_resolution_clock::time_point get_current_time_fenced()
-{
-    std::atomic_thread_fence(memory_order_seq_cst);
-    auto res_time = std::chrono::high_resolution_clock::now();
-    std::atomic_thread_fence(memory_order_seq_cst);
-    return res_time;
-}
-
-template<class D>
-inline long long to_us(const D& d)
-{
-    return std::chrono::duration_cast<chrono::microseconds>(d).count();
-}
-//Write my func
-double func_calculation(double x1, double x2, int m) {
+double func_calculation(int m, double x1, double x2) {
 //    double sum1 = 0;
 //    double sum2 = 0;
 //    double g;
@@ -38,35 +26,29 @@ double func_calculation(double x1, double x2, int m) {
 //    g = - sum1 * sum2;
 //
 //    return g;
-    double g,sum = 0.0;
+    double g, sum;
     int j;
-    for (int i = -2; i <= 2; ++i)
-    {
+    for (int i = -2; i <= 2; ++i) {
         j = i;
-        sum += 1 / (5 * (i + 2) + j + 3 + pow(x1 - 16* j,6) + pow(x2 - 16* i,6));
+        sum += 1 / (5 * (i + 2) + j + 3 + pow(x1 - 16 * j, 6) + pow(x2 - 16 * i, 6));
     }
-
     g = pow(0.002 + sum, -1);
-
     return g;
-
 }
 
-double integration(double x0, double x, double y0, double y, int m, double pr) {
+double integrate(double x0, double x, double y0, double y, int m, double pr) {
     assert (m >= 5);
     double sum = 0;
-    for (double i = x0; i <= x; i += pr) for (double j = y0; j <= y; j += pr)
-            sum += func_calculation(i + pr / 2.0, j + pr / 2.0, m) * pr * pr;
+    for (double i = x0; i <= x; i += pr) {
+        for (double j = y0; j <= y; j += pr) {
+            sum += func_calculation(m, i + pr / 2.0, j + pr / 2.0) * pr * pr;
+        }
+    }
     return sum;
 }
 
-void integrateWithThreads(double x0, double x, double y0, double y, int m, double pr, double *r) {
-    double integAnswer = integration(x0, x, y0, y, m, pr);
-    lock_guard<mutex> lg(mx);
-    *r += integAnswer;
-}
 
-template <class T>
+template<class T>
 T get_param(string key, map<string, string> myMap) {
     istringstream ss(myMap[key]);
     T val;
@@ -74,33 +56,60 @@ T get_param(string key, map<string, string> myMap) {
     return val;
 }
 
+void thread_integration(double x0, double x, double y0, double y, int m, double pr, double *r) {
+    auto answer = integrate(x0, x, y0, y, m, pr);
+    lock_guard<mutex> lg(mx);
+    *r += answer;
+}
+
 map<string, string> read_config(string filename) {
     string line, delimiter;
-    ifstream myfile (filename);
+    ifstream myfile(filename);
     map<string, string> mp;
+
     delimiter = "=";
-    if (myfile.is_open())
-    {
-        while (getline(myfile,line))
-        {
+
+
+    if (myfile.is_open()) {
+        while (getline(myfile, line)) {
             int pos = line.find(delimiter);
             string key = line.substr(0, pos);
             string value = line.substr(pos + 1);
             mp[key] = value;
         }
+
         myfile.close();
-    }
-    else {
-        cout << "Cannot open the file!" << endl;
+    } else {
+        cout << "Error with opening the file!" << endl;
     }
     return mp;
 
 };
 
+template<class D>
+inline long long to_us(const D &d) {
+    return std::chrono::duration_cast<chrono::microseconds>(d).count();
+}
 
-int main()
-{
+
+inline std::chrono::high_resolution_clock::time_point get_current_time_fenced() {
+    std::atomic_thread_fence(memory_order_seq_cst);
+    auto res_time = std::chrono::high_resolution_clock::now();
+    std::atomic_thread_fence(memory_order_seq_cst);
+    return res_time;
+}
+
+
+int main() {
+//    MPI_Init(&argc, &argv);
+//    MPI_Comm_size(MPI_COMM_WORLD, &commsize);
+//    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+//    MPI_Get_processor_name(procname, &len);
+
     string filename;
+//    cout << "Please enter name of configuration file with extension '.txt':";
+//    cin >> filename;
+
     filename = "config.txt";
     map<string, string> mp = read_config(filename);
     double abs_er, rel_er, x0, x1, y0, y1;
@@ -114,67 +123,56 @@ int main()
         y1 = get_param<double>("y1", mp);
         m = get_param<int>("m", mp);
         num_of_threads = get_param<int>("threads", mp);
-        cout << "ASD: " << num_of_threads << endl;
+
         thread threads[num_of_threads];
         double pr = 1E-3;
 
         double integral = 0;
         double interval_x = (x1 - x0) / num_of_threads;
         double x = x0;
-        cout << "  Calculating...\n" << endl;
+        cout << "  \t\t\t\t\t\t\t\t\t\t\t\t\tGetting result\n" << endl;
         double step1 = 1E-3;
         double step2 = step1 / 2.0;
-        double integral1 = 0, integral2 = 0;
+        double firtInt = 0, secondInt = 0;
         double j = x, l = x;
 
         while (j < x1) {
-            integral1 += integration(j, j + step1, y0, y1, m, pr);
+            firtInt += integrate(j, j + step1, y0, y1, m, pr);
             j += step1;
         }
 
         while (l < x1) {
-            integral2 += integration(l, l + step2, y0, y1, m, pr);
+            secondInt += integrate(l, l + step2, y0, y1, m, pr);
             l += step2;
         }
 
-        double abs_dif = abs(integral1 - integral2);
-        double rel_dif = abs((integral1 - integral2) / max(integral1, integral2));
+        double abs_dif = abs(firtInt - secondInt);
+        double rel_dif = abs((firtInt - secondInt) / max(firtInt, secondInt));
 
-        if (abs_dif <= abs_er)
-            cout << "| Absolute error is okay\t";
-        else
-            cout << "| Absolute error is not okay\t";
-
-        cout << abs_dif << " vs " << abs_er << endl;
-
-        if (rel_dif <= rel_er)
-            cout << "| Relative error is okay\t";
-        else
-            cout << "| Relative error is not okay\t";
-        cout << rel_dif << " vs " << rel_er << endl;
 
         auto start_time_reading = get_current_time_fenced();
         for (int i = 0; i < num_of_threads; ++i) {
-            threads[i] = thread(integrateWithThreads, x, x + interval_x, y0, y1, m, pr, &integral);
+            threads[i] = thread(thread_integration, x, x + interval_x, y0, y1, m, pr, &integral);
             x += interval_x;
         }
 
-        for (int i = 0; i < num_of_threads; ++i) threads[i].join();
+        for (int count = 0; count < num_of_threads; ++count) threads[count].join();
 
-        auto finish_time = get_current_time_fenced();
-        auto total_time = finish_time - start_time_reading;
+        auto ending_time = get_current_time_fenced();
+        auto total_time = ending_time - start_time_reading;
 
-        cout << " -------------------------------------\n| Time: " << to_us(total_time)
-             << " ms\n -------------------------------------" << endl;
-        ofstream result, additionalInfo;
+        cout << "Absolute error: real vs desirable - " << abs_dif << " vs " << abs_er << endl;
+        cout << "Relative error: real vs desirable - " << rel_dif << " vs " << rel_er << endl;
+        cout << "Total time: " << to_us(total_time) << endl;
+
+        ofstream result, additionalInf;
         result.open("result.txt");
-        additionalInfo.open("additionalInfo.txt");
-              result << integral << endl;
-        additionalInfo << "| Absolute error: " << abs_dif << endl;
-        additionalInfo << "| Relative error: " << rel_dif << "\n|-----------------------------" << endl;
-        additionalInfo << "| Total time: " << total_time.count() << " ms\n -----------------------------" << endl;
+        additionalInf.open("additionInf.txt");
+        additionalInf << "Integral: " <<integral << endl;
+        additionalInf << "Absolute error: " << abs_dif << endl;
+        additionalInf << "Relative error: " << rel_dif << endl;
+        result << total_time.count() << endl;
 
-        additionalInfo << "\t|  Threads counted: " << integral << endl;
     }
     return 0;
 }
